adb1606b8d5990c094db3f7803bd361a7687e7e5
SevakOhanian7 <sevakohanian7@gmail.com>
2021-02-10 17:52:59 -0800

    Committing rest of Select classes including Not, And, and Or

--- select.hpp
+++ select.hpp
@@ -10,6 +10,7 @@ public:
 
     // Return true if the specified row should be selected.
     virtual bool select(int row) const = 0;
+    virtual int getRowSize() const = 0;
 };
 
 // A common type of criterion for selection is to perform a comparison based on
@@ -25,6 +26,7 @@ class Select_Contains: public Select
 protected:
 	int column;
 	bool* chosenRows;
+	int numRows;
 
 public:
 
@@ -38,11 +40,12 @@ public:
 		for(int i = 0; i < sheet->get_row_size(); i++)
 			chosenRows[i] = false;
 		
+		numRows = sheet->get_row_size();
 
 		column = sheet->get_column_by_name(col);
 		if(column != -1){
 			for(int i = 0; i < sheet->get_row_size(); i++){
-				if((sheet->cell_data(i, column)) == content){
+				if((sheet->cell_data(i, column)).find(content) != std::string::npos){
 					setSelection(i);
 				}
 			}
@@ -58,7 +61,122 @@ public:
         	return chosenRows[row];
 	}
 
+	virtual int getRowSize() const {
+		return numRows;
+	}
 
+
+};
+
+class Select_Not: public Select
+{
+protected:
+	bool* chosenRows;
+	int rows;
+
+public:
+	~Select_Not() {
+		delete chosenRows;
+	}
+
+	Select_Not(Select* first){
+		
+		rows = first->getRowSize();
+		chosenRows = new bool[rows];		
+
+		for(int i = 0; i < rows; i++){
+			if(!first->select(i)){
+				setSelection(i);	
+			}
+		} 
+	}
+
+	void setSelection(int row) {
+		chosenRows[row] = true;
+	}
+
+
+	virtual bool select(int row) const {
+		return chosenRows[row];
+	}
+
+	virtual int getRowSize() const {
+		return rows;
+	}
+};
+
+class Select_And: public Select
+{
+protected:
+        bool* chosenRows;
+        int rows;
+
+public:
+        ~Select_And() {
+                delete chosenRows;
+        }
+
+        Select_And(Select* first, Select* second){
+
+                rows = first->getRowSize();
+                chosenRows = new bool[rows];
+
+                for(int i = 0; i < rows; i++){
+                        if(first->select(i) && second->select(i)){
+                                setSelection(i);
+                        }
+                }
+        }
+
+        void setSelection(int row) {
+                chosenRows[row] = true;
+        }
+
+
+        virtual bool select(int row) const {
+                return chosenRows[row];
+        }
+
+        virtual int getRowSize() const {
+                return rows;
+        }
+};
+
+class Select_Or: public Select
+{
+protected:
+        bool* chosenRows;
+        int rows;
+
+public:
+        ~Select_Or() {
+                delete chosenRows;
+        }
+
+        Select_Or(Select* first, Select* second){
+
+                rows = first->getRowSize();
+                chosenRows = new bool[rows];
+
+                for(int i = 0; i < rows; i++){
+                        if(first->select(i) || second->select(i)){
+                                setSelection(i);
+                        }
+                }
+        }
+
+        void setSelection(int row) {
+                chosenRows[row] = true;
+        }
+
+
+        virtual bool select(int row) const {
+                return chosenRows[row];
+        }
+
+        virtual int getRowSize() const {
+                return rows;
+        }
 };
 
 
